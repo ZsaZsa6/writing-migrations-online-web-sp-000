c0b133b494d491a766a12d816db931acb6562b07
Ronda D Cowen <cowenhome5@gmail.com>
2020-03-25 17:10:20 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def create_table
-    add_column :artists, :grade, :integer
-    
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :
   end
 end
